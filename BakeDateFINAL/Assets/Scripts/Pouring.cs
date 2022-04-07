bee34e9bb8164b00870bf8cb3f8c827c4c9743b0
Naomi Matthews <67332327+naomimatthews@users.noreply.github.com>
2022-04-07 15:49:17 +0100

    teleporters

--- BakeDateFINAL/Assets/Scripts/Pouring.cs
+++ BakeDateFINAL/Assets/Scripts/Pouring.cs
@@ -8,24 +8,20 @@ public class Pouring : MonoBehaviour
     [SerializeField] public GameObject smallMilk;
     [SerializeField] public GameObject mediumMilk;
     [SerializeField] public GameObject bigMilk;
-    [SerializeField] public GameObject liquid;
+    [SerializeField] public GameObject partEffect;
 
     bool pouring = false;
     bool triggerValue;
 
     void Start()
     {
-        var gameControllers = new List<UnityEngine.XR.InputDevice>();
-        UnityEngine.XR.InputDevices.GetDevicesWithRole(UnityEngine.XR.InputDeviceRole.GameController.gameControllers);
+     
 
-        if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
-        {
-            Debug.Log("Trigger button is pressed!");
+    }
 
-            // GameObject newLiquid = Instantiate(liquid, trnsform.down, transform.rotation);
-            //newLiquid.GetComponent<Rigidbody>().AddForce(transform.down, ForceMode.Impulse);
-        }
-
+    private void TriggerPressed()
+    {
+        // trigger pressed
     }
 
     public void smallMilkPouring()
