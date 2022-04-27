d6ef467644704c409713056b78742327db39d89a
Naomi Matthews <67332327+naomimatthews@users.noreply.github.com>
2022-04-27 15:35:23 +0100

    idk

--- BakeDateFINAL/Assets/Scripts/Pouring.cs
+++ BakeDateFINAL/Assets/Scripts/Pouring.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class Pouring : MonoBehaviour
 {
@@ -8,20 +10,44 @@ public class Pouring : MonoBehaviour
     [SerializeField] public GameObject smallMilk;
     [SerializeField] public GameObject mediumMilk;
     [SerializeField] public GameObject bigMilk;
-    [SerializeField] public GameObject partEffect;
+    [SerializeField] public GameObject pourEffect;
 
     bool pouring = false;
     bool triggerValue;
 
-    void Start()
-    {
-     
+    Vector3 oldRotation;
 
+    GenericXRController inputActions;
+
+    private void Awake()
+    {
+        inputActions = new GenericXRController();
+        inputActions.Enable();
     }
 
-    private void TriggerPressed()
+    void Start()
     {
-        // trigger pressed
+        SteamVR_Actions.default_GrabPinch.AddOnStateDownListener(TriggerPressed, SteamVR_Input_Sources.Any);
+        oldRotation = GameObject.transform.rotation.eulerAngles;
+    }
+
+    foreach (var device in inputDevices)
+    {
+        Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
+    }
+
+    private void TriggerPressed(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+
+        if(oldRotation == GameObject.transform.rotation.eulerAngles)
+        {
+            // no change
+        }
+        else
+        {
+            oldRotation = GameObject.transform.rotation.eulerAngles;
+            pourEffect = Instantiate(pourEffect, transform.position, transform.rotation);
+        }
     }
 
     public void smallMilkPouring()
